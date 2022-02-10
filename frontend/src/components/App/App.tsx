b7d3b793628f8a21897bfce5545e86720cb318c4
Andrew Lorber <andrewlorber@aol.com>
2022-02-09 23:30:41 -0500

    Adds React Router

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
 import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Stack } from '@chakra-ui/react';
 import './App.css';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Stack className='App' h={'100%'}>
+        <Routes>
+          <Route path='/' element={<></>} />
+          <Route path='/sell' element={<></>} />
+        </Routes>
+      </Stack>
+    </BrowserRouter>
   );
 }
 
