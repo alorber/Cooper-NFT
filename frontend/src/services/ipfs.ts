91a4e8755eadfb714eed94c39db8414e51e21087
Andrew Lorber <andrewlorber@aol.com>
2022-02-23 12:09:36 -0500

    Retrieves NFT from IPFS & determines file type

--- frontend/src/services/ipfs.ts
+++ frontend/src/services/ipfs.ts
@@ -1,15 +1,26 @@
-// Functions to access IPFS
-import {CID, create as ipfsHttpClient } from 'ipfs-http-client';
-import { IPFS_PROJECT_ID, IPFS_PROJECT_SECRET } from './IPFS_AUTH';
-import {base16} from 'multiformats/bases/base16';
+import { base16 } from 'multiformats/bases/base16';
+import { CID, create as ipfsHttpClient } from 'ipfs-http-client';
 import { Failure } from './contracts';
+import { fromBuffer, FileExtension, MimeType } from 'file-type';
+import { IPFS_PROJECT_ID, IPFS_PROJECT_SECRET } from './IPFS_AUTH';
 
 // Type Declarations
 // -------------------
 
-export type ipfsResponse = {
+export type NFTMetadata = {
+    name: string,
+    description: string,
+    image: string
+}
+
+export type IpfsMetadataResponse = {
     status: "Success",
-    file: string
+    metadata: NFTMetadata
+} | Failure;
+
+export type IpfsNftResponse = {
+    status: "Success",
+    file: File
 } | Failure;
 
 // Creates Auth Header
@@ -25,7 +36,7 @@ const client = ipfsHttpClient({
 
 // Uploads file to IPFS & returns CID
 export const uploadFileToIPFS = async (file: File, hash: string = 'blake2b-208') => {
-    const addedFile = await client.add(file, {cidVersion: 1, hashAlg: hash, pin: true});
+    const addedFile = await client.add(file, {cidVersion: 1, hashAlg: hash, pin: true, wrapWithDirectory: false});
     const cid = addedFile.cid;
     return cid;
 }
@@ -36,27 +47,46 @@ export const cidToBase16 = (cid: CID) => {
 }
 
 // Builds metadata
-export type NFTMetadata = {
-    name: string,
-    description: string,
-    image: string
-}
 export const buildNFTMetadata = (data: NFTMetadata) => {
     return JSON.stringify(data);
 }
 
-// Retrieves file from IPFS
-export const getFileFromIPFS = async (uri: string): Promise<ipfsResponse> => {
+// Retrieves Metadata from IPFS
+export const getMetadataFromIPFS = async (uri: string): Promise<IpfsMetadataResponse> => {
     try {
-        const retrievedFile = await client.cat(uri);
         const decoder = new TextDecoder();
-        let decodedFile = '';
-        for await (const chunk of retrievedFile) {
-            decodedFile += decoder.decode(chunk);
+        let decodedMetadata = '';
+        for await (const chunk of client.cat(uri)) {
+            decodedMetadata += decoder.decode(chunk);
         }
-        return {status: "Success", file: decodedFile};
+        return {status: "Success", metadata: JSON.parse(decodedMetadata)};
     } catch(err: any) {
         return {status: "Failure", error: err};
     }
     
+}
+
+// Retrieves Metadata from IPFS
+export const getNftFromIPFS = async(uri: string, fileName: string = 'a'): Promise<IpfsNftResponse> => {
+    try {
+        // Determines file type
+        let fileType: {ext: FileExtension, mime: MimeType} | undefined = undefined;
+        for await(const chunk of client.cat(uri, {length: 100})) {
+            fileType = await fromBuffer(chunk);
+        }
+        // Assume image if trouble determining
+        if(fileType === undefined) {
+            fileType = {ext: 'jpg', mime: 'image/jpeg'};
+        }
+        
+        // Retrieves file
+        let blobs: Blob[] = [];
+        for await (const chunk of client.cat(uri)) {
+            blobs.push(new Blob([chunk]))
+        }
+        const file = new File(blobs, `${fileName}.${fileType?.ext}`, {type: fileType.mime});
+        return {status: "Success", file: file}
+    } catch(err: any) {
+        return {status: "Failure", error: err};
+    }
 }
