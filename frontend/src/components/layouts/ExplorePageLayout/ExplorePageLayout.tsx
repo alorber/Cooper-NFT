063d4132996c850656946200959667f77e060de6
Andrew Lorber <andrewlorber@aol.com>
2022-03-29 16:47:25 -0400

    Adds explore page

---
+++ frontend/src/components/layouts/ExplorePageLayout/ExplorePageLayout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type ExplorePageLayoutProps = {
+
+}
+
+const ExplorePageLayout = ({}: ExplorePageLayoutProps) => {
+    return (
+        <></>
+    );
+}
+
+export default ExplorePageLayout;
