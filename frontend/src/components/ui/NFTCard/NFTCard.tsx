308941317abf0a4e40caff89d61883ff82efe8f4
Andrew Lorber <andrewlorber@aol.com>
2022-04-07 23:39:37 -0400

    Adds props to NFT Card

--- frontend/src/components/ui/NFTCard/NFTCard.tsx
+++ frontend/src/components/ui/NFTCard/NFTCard.tsx
@@ -9,31 +9,37 @@ import {
     } from '@chakra-ui/react';
 
 type NFTCardLayoutProps = {
-
+    imageURL: string
+    title: string,
+    creator?: string,
+    price: number,
+    ethToUsdRate: number
 }
 
-const NFTCard = ({}: NFTCardLayoutProps) => {
+const NFTCard = ({imageURL, title, creator, price, ethToUsdRate}: NFTCardLayoutProps) => {
     return (
         <Box w="300px" rounded="20px" overflow="hidden" 
                 bg={ "gray.200" } mt={10}>
-            <Image src= "https://static01.nyt.com/images/2013/02/16/nyregion/JP-COOPER/JP-COOPER-superJumbo.jpg" 
+            <Image src= {imageURL} 
                 boxSize="300px" objectFit={'contain'} backgroundColor='gray.400' />
             <Box p={5}>
                 <Stack>
                     <Heading size={'md'} textAlign='center'>
-                        Title
+                        {title}
                     </Heading>
-                    <Text as='i' textAlign='center' >
-                        Creator
-                    </Text> 
+                    {creator && (
+                        <Text as='i' textAlign='center' >
+                            {creator}
+                        </Text> 
+                    )}
                 </Stack>
                 <HStack alignSelf={'flex-start'} mt={2} mb={6}>
                     <Stack w='50%' alignSelf={'flex-start'} >
                         <Text textAlign='start' >
-                            100 ETH
+                            {price} ETH
                         </Text>
-                        <Text textAlign='start' >
-                            Roughly $300
+                        <Text as='i' textAlign='start' >
+                            (~ ${price * ethToUsdRate})
                         </Text>
                     </Stack>
                     <Stack w='50%' alignSelf={'center'}>
