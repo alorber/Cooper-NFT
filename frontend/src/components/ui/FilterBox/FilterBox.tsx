47f09705d94825359068384a2fb4ca3d83feda61
Andrew Lorber <andrewlorber@aol.com>
2022-04-25 17:02:00 -0400

    Adds filterBy selector

--- frontend/src/components/ui/FilterBox/FilterBox.tsx
+++ frontend/src/components/ui/FilterBox/FilterBox.tsx
@@ -4,10 +4,17 @@ import {
     GridItem,
     Input,
     InputGroup,
-    InputLeftElement
+    InputLeftElement,
+    Select
     } from '@chakra-ui/react';
-import { SearchIcon } from '@chakra-ui/icons';
 import { NFTMarketItem } from '../../../services/marketplace_contract';
+import { SearchIcon } from '@chakra-ui/icons';
+
+enum SortByOptions {
+    NAME = "Name",
+    PRICE = "Price",
+    SELLER = "Seller"
+};
 
 type FilterBoxProps = {
     nftList: NFTMarketItem[],
@@ -16,19 +23,40 @@ type FilterBoxProps = {
 
 const FilterBox = ({nftList, setNftList}: FilterBoxProps) => {
     const [searchTerm, setSearchTerm] = useState('');
+    const [sortKey, setSortKey] = useState<SortByOptions>(SortByOptions.NAME);
 
     // Loads list on load
     useEffect(() => {
         updateSearchResults();
-    }, []);
+    }, [sortKey]);
 
     // Uses current search-term to update search results
     const updateSearchResults = (s: string | null = null) => {
+        console.log("UPDATING SEARCH: ", sortKey)
         const search = s ?? searchTerm;
 
+         // Sorts NFTs by sortBy value - Optional sortkey param
+        const sortNFTs = (nft1: NFTMarketItem, nft2: NFTMarketItem, key?: SortByOptions): number => {
+            key = key ?? sortKey;
+
+            if(sortKey === SortByOptions.NAME) {
+                return nft1.name > nft2.name ? 1 : -1;
+            }
+            if(sortKey === SortByOptions.PRICE) {
+                return nft1.price > nft2.price ? 1 : nft1.price < nft2.price ? -1 : sortNFTs(nft1, nft2, SortByOptions.NAME);
+            }
+            if(sortKey === SortByOptions.SELLER) {
+                return nft1.owner > nft2.owner? 1 : -1;
+            }
+            return 1;
+        }
+
         // Filters NFT List
-        setNftList(nftList.filter(nft => nft.name.includes(search) || search === ''));
-    } 
+        const filteredList = nftList.filter(nft => nft.name.includes(search) || search === '')
+        // Sorts List
+        filteredList.sort(sortNFTs)
+        setNftList(filteredList);
+    }
 
     return (
         <Grid w='80%' templateColumns={'2fr repeat(2,1fr)'} px={6}>
@@ -36,6 +64,9 @@ const FilterBox = ({nftList, setNftList}: FilterBoxProps) => {
             <GridItem>
                 <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} 
                     updateSearchResults={updateSearchResults} />
+            </GridItem>
+            <GridItem>
+                <SortBy setSortKey={setSortKey} />
             </GridItem>
         </Grid>
     )
@@ -67,5 +98,30 @@ const SearchBar = ({searchTerm, setSearchTerm, updateSearchResults}: SearchBarPr
                     onChange={(v)=>{updateSearchTerm(v.target.value)}} />
             </InputGroup>
         </form>
-    )
+    );
 }
+
+// Sort-By Selector Component
+type SortByProps = {
+    setSortKey: (s: SortByOptions) => void
+}
+
+const SortBy = ({setSortKey}: SortByProps) => {
+
+    // Gets SortByOptions enum value from selector value
+    const getEnumValue = (v: string) => {
+        const enumIndex = Object.keys(SortByOptions).indexOf(v);
+        return Object.values(SortByOptions)[enumIndex];
+    }
+
+    return (
+        <Select onChange={v => {setSortKey(getEnumValue(v.target.value))}}>
+            {(Object.keys(SortByOptions) as Array<keyof typeof SortByOptions>).map(sortOption => 
+                <option value={sortOption} key={sortOption}>
+                    {SortByOptions[sortOption]}
+                </option>
+            )}
+        </Select>
+    );
+}
+
