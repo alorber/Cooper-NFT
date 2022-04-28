06607e534df18f13530cb3d933916cb09abed90b
Andrew Lorber <andrewlorber@aol.com>
2022-04-28 02:31:48 -0400

    Displays NFT info on NFT Page

--- frontend/src/components/layouts/NFTPageLayout/NFTPageLayout.tsx
+++ frontend/src/components/layouts/NFTPageLayout/NFTPageLayout.tsx
@@ -1,4 +1,14 @@
 import react, { useEffect, useState } from 'react';
+import {
+    Box,
+    Grid,
+    GridItem,
+    Heading,
+    Image,
+    Stack,
+    Text
+    } from '@chakra-ui/react';
+import { FormSubmitButton } from '../../ui/StyledFormFields/StyledFormFields';
 import { getNFTbyItemId, NFTMarketItem } from '../../../services/marketplace_contract';
 import { parseNFTPageURL, URL_TOKEN_ID_LENGTH } from '../../../services/nftUrls';
 import { useParams } from 'react-router';
@@ -29,6 +39,7 @@ const NFTPageLayout = ({}: NFTPageLayoutProps) => {
         } else {
             console.log("ERROR: ", nftResp.error);
         }
+        setIsLoading(false);
     }
 
     // Parses url on load
@@ -45,12 +56,53 @@ const NFTPageLayout = ({}: NFTPageLayoutProps) => {
             
             loadNFT(parsedUrl.tokenId, parsedUrl.itemId);
         }
-        
-        setIsLoading(false);
     }, [ids]);
 
-    return (
-        <>Token ID: {tokenId}, itemId: {itemId}</>
+    return isLoading ? (
+        <Heading>Loading...</Heading>
+    ) : nft == null ? (
+        <Heading>Unable to load NFT. Try again later.</Heading>
+    ) : (
+        <Grid templateColumns={'repeat(2,auto)'} w='100%' mt={4} p={6}>
+            {/* NFT Image */}
+            <GridItem colSpan={[2,2,1,1]} justifySelf='center'>
+                <Image borderRadius={5} src={URL.createObjectURL(nft.file)}
+                    alt={nft.name} width={["20em","20em","24em", "30em", "40em"]} 
+                    fit="contain" maxW={"1200px"} />
+            </GridItem>
+
+            {/* Title & Description */}
+            <GridItem colSpan={[2,2,1,1]}>
+                <Stack alignItems={'center'} h='100%' justifyContent={'center'}
+                        spacing={6}>
+                    <Heading>
+                        {nft.name}
+                    </Heading>
+                    <Text>
+                        {nft.description}
+                    </Text>
+                </Stack>
+            </GridItem>
+
+            {/* Owner */}
+            <GridItem colSpan={[2,2,1,1]} mt={6}>
+                <Text>
+                    {nft.owner}
+                </Text>
+            </GridItem>
+
+            {/* Price */}
+            <GridItem colSpan={[2,2,1,1]} mt={6}>
+                Price Here
+            </GridItem>
+
+            {/* Purchase Button */}
+            <GridItem colSpan={2} mt={6}>
+                <Box w={'50%'} maxW={'600px'} mx={'auto'}>
+                    <FormSubmitButton isLoading={false} label={'Purchase'} />
+                </Box>
+            </GridItem>
+        </Grid>
     );
 }
 
