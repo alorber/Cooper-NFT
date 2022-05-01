7b119b6094c00f5ff9cd4c52c15c0f4dabba8723
Andrew Lorber <andrewlorber@aol.com>
2022-05-01 02:55:32 -0400

    Adds nft carousel to home page

--- frontend/src/components/layouts/HomePageLayout/HomePageLayout.tsx
+++ frontend/src/components/layouts/HomePageLayout/HomePageLayout.tsx
@@ -1,11 +1,29 @@
-import react from 'react';
+import ImageCarousel, { CarouselNFT } from '../../ui/ImageCarousel/ImageCarousel';
+import react, { useEffect, useState } from 'react';
+import { BACKGROUND_COLOR } from '../../../COLORS';
 import { Box, Heading, Stack } from '@chakra-ui/react';
+import { generateNFTPageURL } from '../../../services/nftUrls';
+import { generateTestData } from '../../../services/testData';
 import { ThemedLinkButton } from '../../ui/ThemedButtons/ThemedButtons';
 
 type HomePageLayoutProps = {
 
 }
 const HomePageLayout = ({}: HomePageLayoutProps) => {
+    const [nftList, setNftList] = useState<CarouselNFT[]>([]);
+
+    const getNFTs = async () => {
+        const testData = await generateTestData();
+        const carouselList: CarouselNFT[] = testData.nftList.map((nft) => ({
+            nftImage: URL.createObjectURL(nft.file),
+            nftPageUrl: generateNFTPageURL(nft.tokenId, nft.itemId)
+        }));
+        setNftList(carouselList);
+    }
+
+    useEffect(() => {
+        getNFTs()
+    }, []);
 
     return (
         <Stack pt={6}>
@@ -20,6 +38,7 @@ const HomePageLayout = ({}: HomePageLayoutProps) => {
                     width={['fit-content', 'fit-content', '20%']} borderRadius={20} 
                     maxWidth={'270px'} hoverTextColor={BACKGROUND_COLOR} />
             </Box>
+            <ImageCarousel title='Recent Listing' nftsList={nftList} />
         </Stack>
     );
 }
